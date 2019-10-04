44b39fdb09fa2d3729f05dacffcd13bb334a1af1
shinketya <shinketya@gmail.com>
2019-10-04 15:25:51 +0900

    Initial Commit

---
+++ TestBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBeacon
+//
+//  Created by shinketya on 2019/10/04.
+//  Copyright © 2019 shinketya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
